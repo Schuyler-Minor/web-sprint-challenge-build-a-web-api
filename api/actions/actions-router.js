8f7d2aa7cffeaf3ce7afbaccf0d709729600e0bb
Schuyler <skym1n0r91@gmail.com>
2022-03-11 13:32:19 -0600

    get projects and actions

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,21 @@
 // Write your "actions" router here!
+const express = require("express");
+const router = express.Router();
+
+const Actions = require("./actions-model");
+
+router.get("/", (req, res) => {
+  console.log(req.query.id);
+  Actions.get(req.query.id)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "actions could not be retrieved",
+      });
+    });
+});
+
+module.exports = router;
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,17 @@
 // Write your "projects" router here!
+const express = require("express");
+const router = express.Router();
+const Projects = require("./projects-model");
+
+router.get("/", (req, res) => {
+  Projects.get(req.query.id)
+    .then((projects) => {
+      res.status(200).json(projects);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ message: "nope" });
+    });
+});
+
+module.exports = router;
